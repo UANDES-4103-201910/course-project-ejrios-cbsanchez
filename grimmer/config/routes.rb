80fad107cbdb76dc5813fbe2d5764af0b979d0a1
Ernesto Ríos <ernestorios@MacBook-Pro-de-Ernesto.local>
2019-05-07 16:10:34 -0400

    otro

--- grimmer/config/routes.rb
+++ grimmer/config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   resources :adminstrators
   resources :super_admins
   get '/users/intro', to: 'users#intro'
+
+  get 'home', to: 'home' ,controller: 'posts'
+  get 'show', to: 'show', controller: 'users'
   get 'new', to: 'new', controller: 'users'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#inicio'
+  get 'LogIn', to: 'LogIn',  controller: 'logins'
+  get 'profile', to: 'profile', controller: 'user_profiles'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#LogIn'
 end
