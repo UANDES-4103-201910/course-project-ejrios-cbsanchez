ce66d51f8c3f2379a00dcbd13eba0f2099268d45
Ernesto Ríos <ernestorios@MacBook-Pro-de-Ernesto.local>
2019-05-07 13:21:23 -0400

    avance

--- grimmer/config/routes.rb
+++ grimmer/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :adminstrators
   resources :super_admins
   get '/users/intro', to: 'users#intro'
+  get 'new', to: 'new', controller: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#prueba'
+  root 'users#inicio'
 end
