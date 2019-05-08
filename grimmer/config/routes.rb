36713f67e5f73fa82d04d89b916de61be2f6f8f6
Ernesto Ríos <ernestorios@MacBook-Pro-de-Ernesto.local>
2019-05-08 07:45:04 -0400

    avance martes

--- grimmer/config/routes.rb
+++ grimmer/config/routes.rb
@@ -12,7 +12,15 @@ Rails.application.routes.draw do
   get 'show', to: 'show', controller: 'users'
   get 'new', to: 'new', controller: 'users'
   get 'LogIn', to: 'LogIn',  controller: 'logins'
-  get 'profile', to: 'profile', controller: 'user_profiles'
+  get 'Profile', to: 'Profile', controller: 'user_profiles'
+  get 'publish', to: 'publish', controller: 'posts'
+  get 'deletePost', to: 'deletePost', controller: 'posts'
+  get 'search_user', to: 'search_user',controller: 'users'
+  get 'uConfig', to: 'uConfig', controller: 'users'
+  get 'LogIn', to: 'LogIn', controller: 'users'
+  get 'full_post', to: 'full_post', controller: 'posts'
+  get 'view_user', to: 'view_user', controller: 'users'
+  get 'blacklist', to: 'blacklist', controller: 'black_lists'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#LogIn'
 end
